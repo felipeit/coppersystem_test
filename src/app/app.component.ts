7aeb9e4f1fce110fbc144e199b38a1912c219ce1
Felipe Silva <felipe.it99@gmail.com>
2021-03-18 22:09:10 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import * as alertify from 'alertifyjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  displayedColumns: string[] = ['name', 'language', 'created_at'];
+
+  constructor(private http: HttpClient) {
+
+  }
+  name = "";
   title = 'github-api';
+  public repositories = "";
+  public pageSlice = "";
+
+  getList() {
+    let url = "https://api.github.com/users/" + this.name + "/repos";
+    if (this.repositories) { this.pageSlice = ""; }
+    this.http.get(url).subscribe((data: any) => {
+      this.repositories = data;
+      this.pageSlice = this.repositories.slice(0, 3)
+
+    }, error => {
+      alertify.error(error.error.message)
+    });
+
+  }
+
+  OnPageChange(event: PageEvent) {
+    console.log(event)
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.repositories.length) {
+      endIndex = this.repositories.length;
+    }
+    this.pageSlice = this.repositories.slice(startIndex, endIndex)
+    console.log(this.repositories);
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    FormsModule,
+    MatPaginatorModule,
+    FlexLayoutModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
